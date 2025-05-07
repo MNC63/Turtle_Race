8db88bc37af5f3cf8d7c6780421a2d17b3de38d0
Pean1112 <nloc8380@gmail.com>
2025-05-07 12:35:41 +0700

    Finish Turtle Race Game by Python

--- Turtle_Race.py
+++ Turtle_Race.py
@@ -1,14 +1,15 @@
 from turtle import Turtle, Screen
 from random import randint
 
-
+Start_race = False
 display = Screen()
 display.setup(width = 800, height = 600)
 
-# #display textinput("Enter your bet: ", "Turtle color")
-# user_choice = display.textinput(title= "Enter your bet: ", prompt ="red, blue, green,...")
-# print(user_choice)
+user_choice = display.textinput(title= "Enter your bet: ", prompt ="red, blue, green,...")
 
+if user_choice:
+    Start_Race = True
+turtle_list = []
 distance = 0
 colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink"]
 
@@ -18,5 +19,17 @@ for i in range(7):
     Race.penup()
     distance += 75
     Race.goto(-380,-300 + distance)
+    turtle_list.append(Race)
+
+while Start_Race:
+    for turtle_each in turtle_list:
+        if turtle_each.xcor() > 370:
+            Start_Race = False
+            wining_clor = turtle_each.pencolor()
+            if wining_clor == user_choice:
+                print(f"You Win")
+            else:
+                print(f"You Lose, ", f"{wining_clor} is the winner")
+        turtle_each.forward(randint(1, 10))
 
 display.exitonclick()
